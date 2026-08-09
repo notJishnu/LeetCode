4a44a64f1d3cce95a52021f6fba956337dfb3690
notJishnu <121246631+notJishnu@users.noreply.github.com>
2026-08-09 20:51:20 +0530

    Time: 33 ms (7.95%), Space: 54 MB (92.46%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,16 +1,38 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
         Set<String> result = new HashSet<>();
+        int k=10;
+        int sum=0;
 
-        for (int i = 0; i <= s.length() - 10; i++) {
-            String dna = s.substring(i, i + 10);
+        Map<Character,Integer> map=new HashMap<>();
+        map.put('A',0);
+        map.put('C',1);
+        map.put('G',2);
+        map.put('T',3);
 
-            if (seen.contains(dna)) {
-                result.add(dna);
-            }
-            seen.add(dna);
+        if(s.length()<=10){
+            return new ArrayList<String>();
         }
+
+        for(int i=0;i<10;i++){
+            int pow=k-i-1;
+            sum=sum+(int)Math.pow(4,pow) * map.get(s.charAt(i));
+        }
+
+        seen.add(sum);
+        
+        for(int i=k;i<s.length();i++){
+            sum=sum-((int)Math.pow(4,k-1) * map.get(s.charAt(i-k)));
+            sum=sum*4;
+            sum+=map.get(s.charAt(i));
+            if(seen.contains(sum)){
+                result.add(s.substring(i-k+1,i+1));
+            }
+            seen.add(sum);
+        }
+
         return new ArrayList<String>(result);
+
     }
 }
