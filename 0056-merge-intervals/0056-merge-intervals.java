a49f68d9b0b3ff717714b168abb51fa1f63ba5ff
notJishnu <121246631+notJishnu@users.noreply.github.com>
2026-08-10 22:58:20 +0530

    Time: 8 ms (91.65%), Space: 48.7 MB (92.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,41 +1,25 @@
 class Solution {
-    public boolean isOverlap(int[] int1,int[] int2){
-        return int1[1]>=int2[0] && int2[1]>=int1[0];
-    }
     public int[][] merge(int[][] intervals) {
-        
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+
         List<int[]> list=new ArrayList<>();
 
-        for(int interval[] : intervals){
-            list.add(interval);
-        }
+        list.add(intervals[0]);
 
-        boolean isMergeHappened=true;
+        for(int i=1;i<intervals.length;i++){
+            int[] prev=list.get(list.size()-1);
+            int[] curr=intervals[i];
 
-        while(isMergeHappened){
-            isMergeHappened=false;
-
-            for(int i=0;i<list.size();i++){
-                int j=i+1;
-
-                while(j<list.size()){
-                    int[] int1=list.get(i);
-                    int[] int2=list.get(j);
-
-                    if(isOverlap(int1,int2)){
-                        int1[0]=Math.min(int1[0],int2[0]);
-                        int1[1]=Math.max(int1[1],int2[1]);
-
-                        list.remove(j);
-                        isMergeHappened=true;
-                    }else{
-                        j++;
-                    }
-                }
+            if(curr[0]<=prev[1] && curr[1]>=prev[0]){
+                prev[0]=Math.min(prev[0],curr[0]);
+                prev[1]=Math.max(prev[1],curr[1]);
+            }else{
+                list.add(intervals[i]);
             }
         }
 
         int[][] res=new int[list.size()][2];
+
         for(int i=0;i<list.size();i++){
             res[i]=list.get(i);
         }
