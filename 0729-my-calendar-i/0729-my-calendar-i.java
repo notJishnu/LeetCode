e7c0e656399e10def8520cedbbd13e59d11b1407
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-08-16 20:03:00 +0530

    Time: 377 ms (5.06%), Space: 47.9 MB (9.73%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,34 +1,34 @@
 class MyCalendar {
+    Map<Integer, Integer> map;
 
-    List<int []> bookings;
     public MyCalendar() {
-        bookings=new ArrayList<int []>();
+        map = new TreeMap<Integer, Integer>();
     }
-    
-    public boolean book(int startTime, int endTime) {
-        if(bookings.size()==0){
-            bookings.add(new int[]{
-                startTime,
-                endTime-1
-            });
-            return true;
-        }
 
-        
-        for(int i=0;i<bookings.size();i++){
-            int s1=bookings.get(i)[0],
-                e1=bookings.get(i)[1],
-                s2=startTime,
-                e2=endTime-1;
-            if(e1>=s2 && e2>=s1){
+    public boolean book(int startTime, int endTime) {
+        map.put(startTime, map.getOrDefault(startTime, 0) + 1);
+        map.put(endTime, map.getOrDefault(endTime, 0) - 1);
+
+        int bookings = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            bookings = bookings + entry.getValue();
+
+            if (bookings > 1) {
+                map.put(startTime, map.get(startTime) - 1);
+                map.put(endTime, map.get(endTime) + 1);
+
+                if(map.get(startTime)==0){
+                    map.remove(startTime);
+                }
+
+                if(map.get(endTime)==0){
+                    map.remove(endTime);
+                }
                 return false;
             }
         }
-        bookings.add(new int[]{
-            startTime,
-            endTime-1
-        });
         return true;
+
     }
 }
 
