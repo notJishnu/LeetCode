016d829f8538d59fe73a3451166db6e06744bb58
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-08-16 19:53:15 +0530

    Time: 100 ms (13.57%), Space: 47.2 MB (70.36%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,36 +1,35 @@
 class MyCalendar {
 
-    List<int[]> bookings;
-
+    List<int []> bookings;
     public MyCalendar() {
-        bookings = new ArrayList<int[]>();
+        bookings=new ArrayList<int []>();
     }
-
+    
     public boolean book(int startTime, int endTime) {
-        if (bookings.size() == 0) {
-            bookings.add(new int[] {
-                    startTime,
-                    endTime - 1
+        if(bookings.size()==0){
+            bookings.add(new int[]{
+                startTime,
+                endTime-1
             });
             return true;
         }
-        for (int i = 0; i < bookings.size(); i++) {
-            int s1 = bookings.get(i)[0],
-                    e1 = bookings.get(i)[1],
-                    s2 = startTime,
-                    e2 = endTime - 1;
-            if (e1 >= s2 && e2 >= s1) {
+
+        
+        for(int i=0;i<bookings.size();i++){
+            int s1=bookings.get(i)[0],
+                e1=bookings.get(i)[1],
+                s2=startTime,
+                e2=endTime-1;
+            if(e1>=s2 && e2>=s1){
                 return false;
             }
-
         }
-        bookings.add(new int[] {
-                startTime,
-                endTime - 1
+        bookings.add(new int[]{
+            startTime,
+            endTime-1
         });
         return true;
     }
-
 }
 
 /**
