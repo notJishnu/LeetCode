1c07a5599626d4c07a4756a732bf6f4a71ae7bb8
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-08-29 21:26:43 +0530

    Time: 0 ms (100%), Space: 43.1 MB (9.97%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,11 +1,9 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        return subsetXor(nums,0,0);
-    }
-    public int subsetXor(int[] nums,int index,int xor){
-        if(index==nums.length) return xor;
-        int pick=subsetXor(nums, index+1, xor ^ nums[index]);
-        int nopick=subsetXor(nums, index+1, xor);
-        return pick+nopick;
+        int totalXor=0;
+        for(int num: nums){
+            totalXor=totalXor | num;
+        }
+        return totalXor * (int)Math.pow(2,nums.length-1);
     }
 }
