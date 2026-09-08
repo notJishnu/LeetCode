8f59fcc894711f4cdf216c02bb33c540aa9fec88
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-09-08 18:55:07 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (85.26%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,19 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        int ans = 0;
-        int low = 1;  
-        int high = 9;  
-        int d = 1;
-        while (low <= n) {
-            int count = Math.min(n, high) - low + 1;
-            int commasPerNumber = (d - 1) / 3;
-            ans += count * commasPerNumber;
-
-            low = high + 1;
-            high = high * 10 + 9;
-            d++;
+        if(n<1000){
+            return 0;
         }
-
-        return ans;
+        return n-999;
     }
 }
