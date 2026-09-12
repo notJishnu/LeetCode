c75b4f709cf01c09cb88b554182ab2fc166e9bb3
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-09-12 23:31:53 +0530

    Time: 3 ms (73.87%), Space: 69.8 MB (25.7%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -4,14 +4,13 @@ class Solution {
             maxSum=0;
 
         for(int i=0;i<k;i++){
-            sum+=nums[i];
+            sum += nums[i];
         }
         maxSum=sum;
         for(int j=k;j<nums.length;j++){
-            sum=sum-nums[j-k]+nums[j];
-            maxSum=Math.max(maxSum,sum);
+            sum = sum - nums[j-k] + nums[j];
+            maxSum=Math.max(sum,maxSum);
         }
         return (double)maxSum/k;
-
     }
 }
