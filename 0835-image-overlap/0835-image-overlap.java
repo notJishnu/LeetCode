ff55be22fe9c86c09e32272a77deb0825378c5aa
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-09-13 14:15:39 +0530

    Time: 56 ms (65.53%), Space: 44.3 MB (89.02%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,32 +1,33 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n = img1.length;
-        List<int[]> A = new ArrayList<>();
-        List<int[]> B = new ArrayList<>();
+        int max = 0;
 
-        // Step 1 & 2: collect coordinates of 1s in each image
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (img1[r][c] == 1) A.add(new int[]{r, c});
-                if (img2[r][c] == 1) B.add(new int[]{r, c});
+        for (int r = -n + 1; r < n; r++) {
+            for (int c = -n + 1; c < n; c++) {
+
+                int overlap = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        int x = i + r;
+                        int y = j + c;
+
+                        if (x >= 0 && x < n &&
+                            y >= 0 && y < n &&
+                            img1[i][j] == 1 &&
+                            img2[x][y] == 1) {
+
+                            overlap++;
+                        }
+                    }
+                }
+
+                max = Math.max(max, overlap);
             }
         }
 
-        // Step 3: count offsets between every pair (a in A, b in B)
-        Map<String, Integer> offsetCount = new HashMap<>();
-        int maxOverlap = 0;
-
-        for (int[] a : A) {
-            for (int[] b : B) {
-                int dr = b[0] - a[0];
-                int dc = b[1] - a[1];
-                String key = dr + "," + dc;
-                int newCount = offsetCount.getOrDefault(key, 0) + 1;
-                offsetCount.put(key, newCount);
-                maxOverlap = Math.max(maxOverlap, newCount);
-            }
-        }
-
-        return maxOverlap;
+        return max;
     }
 }
