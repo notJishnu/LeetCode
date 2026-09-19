c05a2feab5ce74e1b928159ef7d2bd7dee980f23
Jishnu Guha <121246631+notJishnu@users.noreply.github.com>
2026-09-19 19:26:07 +0530

    Time: 1 ms (99.92%), Space: 47.2 MB (87.65%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,47 +1,17 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        int max=Integer.MIN_VALUE;
-        int index=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
-                index=i;
-            }else{
-                break;
-            }
+        int i=0,
+            n=arr.length;
+
+        while(i+1<n && arr[i+1]>arr[i]){
+            i++;
         }
-        if(index==0 || index==arr.length-1){
+        if(i==0 || i==n-1){
             return false;
         }
-        
-        int i=index,
-            j=index;
-        while(i>0 && j<arr.length-1){
-            if(arr[i-1]<arr[i]){
-                i--;
-            }else{
-                return false;
-            }
-            if(arr[j+1]<arr[j]){
-                j++;
-            }else{
-                return false;
-            }
+        while(i+1<n && arr[i+1]<arr[i]){
+            i++;
         }
-        while(i>0){
-            if(arr[i-1]<arr[i]){
-                i--;
-            }else{
-                return false;
-            }
-        }
-        while(j<arr.length-1){
-            if(arr[j+1]<arr[j]){
-                j++;
-            }else{
-                return false;
-            }
-        }
-        return true;
+        return i==n-1;
     }
 }
